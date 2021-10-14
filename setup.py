c2a34654ae46823b90b90ea4d8cdee25a5dde9c8
Sagar Chamling <sgr.raee@gmail.com>
2021-10-15 02:00:20 +0545

    Setup github action

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
 
+__version__ = "0.1.0"
+
 setup(
     name="ipo",
+    version=__version__,
+    author="Sagar Tamang, Sagar Chamling",
+    author_email="mi5t4n@gmail.com, sgr.raee@gmail.com",
     packages=["ipo_checker"],
     entry_points={
         "console_scripts": [
